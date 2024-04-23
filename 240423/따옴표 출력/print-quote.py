a33a3c3ef2d93258a53e0cdc12239e2360281453
nanji625 <160685081+nanji625@users.noreply.github.com>
2024-04-23 12:36:06 +0900

    print-quote.py

--- 240423/따옴표 출력/print-quote.py
+++ 240423/따옴표 출력/print-quote.py
@@ -1,1 +1,4 @@
-print('He says \"It\'s a really simple sentence\".')
+print('He says \'It\'s a really simple sentence\'.')
+
+# print('He says "It\'s a really simple sentence".')
+# print('''He says 'It's a really simple sentence'.''')
