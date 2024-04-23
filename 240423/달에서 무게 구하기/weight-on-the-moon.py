c65ff3fa6ecdcaf9ffa58a2bc48f8302421a7912
nanji625 <160685081+nanji625@users.noreply.github.com>
2024-04-23 16:28:15 +0900

    Title: 달에서 무게 구하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240423/달에서 무게 구하기/weight-on-the-moon.py
@@ -0,0 +1,11 @@
+choo = 13
+p_dal = 0.165
+
+# 풀이 1
+# print('%d * %.6f = %.6f' % (choo, p_dal, choo*p_dal))
+
+# 풀이 2
+# print('{} * {:.6f} = {:.6f}'.format(choo, p_dal, choo*p_dal))
+
+# 풀이 3
+print('{0} * {1:.6f} = {2:.6f}'.format(choo, p_dal, choo*p_dal))
